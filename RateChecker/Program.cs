467e0996be0ecfa0f7b2c70c5cf9207284c74de0
Aram Osipyan <aosipyan@provenworks.com>
2023-09-16 21:30:31 +0300

    init commit

---
+++ RateChecker/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RateChecker;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.WebHost.UseUrls(urls: "https://*:7035");
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+
+startup.Configure(app, builder.Environment, args);
---
+++ RateChecker/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace RateChecker;
+public sealed class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration) => _configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        var connectionString = _configuration["ConnectionString"];
+
+        /*services
+            .AddFluentMigrator(
+                connectionString,
+                typeof(SqlMigration).Assembly);*/
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env, string[] args)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        var accessKey = _configuration["AccessKey"];
+        RunWithMigrate(app, args);
+
+    }
+
+    private static void RunWithMigrate(IHost host, string[] args)
+    {
+        if (args.Length > 0 && args[0].Equals("migrate", StringComparison.InvariantCultureIgnoreCase))
+        {
+            using var scope = host.Services.CreateScope();
+            // var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+            //runner.MigrateUp();
+        }
+        else
+            host.Run();
+    }
+}
