389fb25bcc2fab2ae1a71763f6f2b9dcf61107e4
Aram Osipyan <aosipyan@provenworks.com>
2023-10-12 07:07:59 +0300

    make delay wait until all requests will close

--- RateChecker/SeleniumServices/States/TokenFetching.cs
+++ RateChecker/SeleniumServices/States/TokenFetching.cs
@@ -40,7 +40,6 @@ public class TokenFetching : State<StateMachineContext, TriggerEnum, StateEnum>
             
 
             var searchReq = new TaskCompletionSource<(string token, string cookie)>();
-            var requestsProccessed = new TaskCompletionSource<bool>();
             int counter = 0;
 
 
@@ -64,22 +63,19 @@ public class TokenFetching : State<StateMachineContext, TriggerEnum, StateEnum>
             network.NetworkResponseReceived += (sender, e) =>
             {
                 Interlocked.Decrement(ref counter);
-
-
-                if (searchReq.Task.IsCompleted && counter == 0)
-                {
-                    requestsProccessed.TrySetResult(true);
-                    network.StopMonitoring().Wait();
-                }
             };
             
 
 
             driver.Navigate().GoToUrl("https://p2p.binance.com/ru/trade/all-payments/USDT?fiat=RUB");
 
-            Task.Delay(10000).Wait();
+            Task.Delay(15000).Wait();
+
+
+            SpinWait.SpinUntil(() => counter == 0);
+
+            network.StopMonitoring().Wait();
             searchReq.Task.Wait();
-            requestsProccessed.Task.Wait();
 
             var result = searchReq.Task.Result;
 
